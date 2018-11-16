c45230a8dc035db67ad629bbebdf9158830a9d5a
aoalaka <abdulquadrialaka@gmail.com>
2018-11-16 12:45:23 +0100

    preferences added

--- app/src/main/java/com/example/android/newsfeed/NewsFeedActivity.java
+++ app/src/main/java/com/example/android/newsfeed/NewsFeedActivity.java
@@ -1,31 +1,51 @@
 package com.example.android.newsfeed;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class NewsFeedActivity extends AppCompatActivity {
-    /** URL for earthquake data from the USGS dataset */
-    private static final String GUARDIAN_REQUEST_URL =
-            "https://content.guardianapis.com/search?section=education" +
-                    "&order-by=newest&show-tags=contributor&page-size=10&api-key=78aa14a6-b3b2-48a8-bf4e-1f85088ff7dd";
+import static com.example.android.newsfeed.QueryUtils.LOG_TAG;
 
-    /** Adapter for the list of newsfeeds */
+public class NewsFeedActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<NewsFeed>> {
+    /**
+     * URL for earthquake data from the USGS dataset
+     */
+    private static final String GUARDIAN_REQUEST_URL =
+            "https://content.guardianapis.com/search";
+
+    /**
+     * Adapter for the list of newsfeeds
+     */
     private NewsFeedAdapter mAdapter;
+
+    /**
+     * Constant value for the earthquake loader ID. We can choose any integer.
+     * This really only comes into play if you're using multiple loaders.
+     */
+    private static final int EARTHQUAKE_LOADER_ID = 1;
 
     /**
      * TextView that is displayed when the list is empty
      */
-//    private TextView emptyStateTextView;
+    private TextView emptyStateTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,8 +54,8 @@ public class NewsFeedActivity extends AppCompatActivity {
         // Find a reference to the {@link ListView} in the layout
         ListView newsfeedListView = (ListView) findViewById(R.id.list);
 
-        /*emptyStateTextView = (TextView) findViewById(R.id.empty_state_text_view);
-        newsfeedListView.setEmptyView(emptyStateTextView);*/
+        emptyStateTextView = (TextView) findViewById(R.id.empty_state_text_view);
+        newsfeedListView.setEmptyView(emptyStateTextView);
 
         // Create a new adapter that takes an empty list of newsfeeds as input
         mAdapter = new NewsFeedAdapter(this, new ArrayList<NewsFeed>());
@@ -63,61 +83,100 @@ public class NewsFeedActivity extends AppCompatActivity {
             }
         });
 
-        // Start the AsyncTask to fetch the earthquake data
-        NewsFeedAsyncTask task = new NewsFeedAsyncTask();
-        task.execute(GUARDIAN_REQUEST_URL);
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        // If there is a network connection, fetch data
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // Get a reference to the LoaderManager, in order to interact with loaders.
+            android.app.LoaderManager loaderManager = getLoaderManager();
+
+            // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+            // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+            // because this activity implements the LoaderCallbacks interface).
+            loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
+        } else {
+            // Otherwise, display error
+            // First, hide loading indicator so error message will be visible
+            View loadingIndicator = findViewById(R.id.loading_indicator);
+            loadingIndicator.setVisibility(View.GONE);
+
+            // Update empty state with no connection error message
+            emptyStateTextView.setText(R.string.no_internet_connection);
+        }
+        Log.i(LOG_TAG, "Earthquake Activity onCreate() called ...");
+
     }
 
-    /**
-     * {@link AsyncTask} to perform the network request on a background thread, and then
-     * update the UI with the list of earthquakes in the response.
-     *
-     * AsyncTask has three generic parameters: the input type, a type used for progress updates, and
-     * an output type. Our task will take a String URL, and return an NewsFeed. We won't do
-     * progress updates, so the second generic is just Void.
-     *
-     * We'll only override two of the methods of AsyncTask: doInBackground() and onPostExecute().
-     * The doInBackground() method runs on a background thread, so it can run long-running code
-     * (like network activity), without interfering with the responsiveness of the app.
-     * Then onPostExecute() is passed the result of doInBackground() method, but runs on the
-     * UI thread, so it can use the produced data to update the UI.
-     */
-    private class NewsFeedAsyncTask extends AsyncTask<String, Void, List<NewsFeed>> {
+    @Override
+    // onCreateLoader instantiates and returns a new Loader for the given ID
+    public Loader<List<NewsFeed>> onCreateLoader(int i, Bundle bundle) {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
 
-        /**
-         * This method runs on a background thread and performs the network request.
-         * We should not update the UI from a background thread, so we return a list of
-         * {@link NewsFeed}s as the result.
-         */
-        @Override
-        protected List<NewsFeed> doInBackground(String... urls) {
-            // Don't perform the request if there are no URLs, or the first URL is null.
-            if (urls.length < 1 || urls[0] == null) {
-                return null;
-            }
+        // getString retrieves a String value from the preferences. The second parameter is the default value for this preference.
+        String section = sharedPrefs.getString(
+                getString(R.string.settings_section_key),
+                getString(R.string.settings_section_default));
+        String tag = sharedPrefs.getString(
+                getString(R.string.settings_tag_key),
+                getString(R.string.settings_tag_default));
+        String pageSize = sharedPrefs.getString(
+                getString(R.string.settings_page_size_key),
+                getString(R.string.settings_page_size_default));
+        String orderBy  = sharedPrefs.getString(
+                getString(R.string.settings_order_by_key),
+                getString(R.string.settings_order_by_default)
+        );
 
-            List<NewsFeed> result = QueryUtils.fetchNewsFeedData(urls[0]);
-            return result;
+        // parse breaks apart the URI string that's passed into its parameter
+        Uri baseUri = Uri.parse(GUARDIAN_REQUEST_URL);
+
+        // buildUpon prepares the baseUri that we just parsed so we can add query parameters to it
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        // Append query parameter and its value. For example, the `section=technology`
+        uriBuilder.appendQueryParameter("section", section);
+        uriBuilder.appendQueryParameter("tag", tag);
+        uriBuilder.appendQueryParameter("show-tags", "contributor");
+        uriBuilder.appendQueryParameter("page-size", pageSize);
+        uriBuilder.appendQueryParameter("order-by", "newest");
+        uriBuilder.appendQueryParameter("order-by", orderBy);
+        uriBuilder.appendQueryParameter("api-key", "78aa14a6-b3b2-48a8-bf4e-1f85088ff7dd");
+
+
+
+        // Return the completed uri `http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&limit=10&minmag=minMagnitude&orderby=time
+        return new NewsFeedLoader(this, uriBuilder.toString());
+
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<NewsFeed>> loader, List<NewsFeed> newsfeeds) {
+        // Hide loading indicator because the data has been loaded
+        View loadingIndicator = findViewById(R.id.loading_indicator);
+        loadingIndicator.setVisibility(View.GONE);
+        // Set empty state text to display "No news found."
+        emptyStateTextView.setText(R.string.no_newsfeeds_found);
+        // Clear the adapter of previous earthquake data
+        mAdapter.clear();
+
+        // If there is a valid list of {@link NewsFeed}s, then add them to the adapter's
+        // data set. This will trigger the ListView to update.
+        if (newsfeeds != null && !newsfeeds.isEmpty()) {
+            mAdapter.addAll(newsfeeds);
         }
+        Log.i(LOG_TAG, "OnLoadFinished() called ...");
+    }
 
-        /**
-         * This method runs on the main UI thread after the background work has been
-         * completed. This method receives as input, the return value from the doInBackground()
-         * method. First we clear out the adapter, to get rid of earthquake data from a previous
-         * query to GUARDIAN. Then we update the adapter with the new list of earthquakes,
-         * which will trigger the ListView to re-populate its list items.
-         */
-        @Override
-        protected void onPostExecute(List<NewsFeed> data) {
-            // Clear the adapter of previous earthquake data
-            mAdapter.clear();
-
-            // If there is a valid list of {@link NewsFeed}s, then add them to the adapter's
-            // data set. This will trigger the ListView to update.
-            if (data != null && !data.isEmpty()) {
-                mAdapter.addAll(data);
-            }
-        }
+    @Override
+    public void onLoaderReset(Loader<List<NewsFeed>> loader) {
+        // Loader reset, so we can clear out our existing data.
+        mAdapter.clear();
+        Log.i(LOG_TAG, "OnLoaderReset() called ...");
     }
 
     @Override
@@ -126,5 +185,16 @@ public class NewsFeedActivity extends AppCompatActivity {
         // Inflate the Options Menu we specified in XML
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
